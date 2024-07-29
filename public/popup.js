4e5ced08ce707f73766eed0555a8d0b17991e593
evynxxz <evyn330@gmail.com>
2024-07-29 22:50:58 +0100

    Create popup.js

---
+++ public/popup.js
@@ -0,0 +1,13 @@
+document.getElementById('openPopup').addEventListener('click', function() {
+    document.getElementById('popup').classList.add('show');
+});
+
+document.getElementById('closePopup').addEventListener('click', function() {
+    const popup = document.getElementById('popup');
+    popup.style.opacity = '0';
+    setTimeout(() => {
+        popup.classList.remove('show');
+        popup.style.opacity = '';
+    }, 300); 
+});
+
